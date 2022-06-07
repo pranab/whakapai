0d6a11a57174229363f640fcfefc16105c9e9aea
Pranab Ghosh <pkghosh99@gmail.com>
2022-06-06 22:17:13 -0700

    exp3 MAB algorithm

--- qinisa/qinisa/mab.py
+++ qinisa/qinisa/mab.py
@@ -191,7 +191,7 @@ class ThompsonSampling(MultiArmBandit):
 		reward feedback for action
 			
 		Parameters
-			act : action
+			aname : action name
 			reward : reward value
 		"""
 		super().setReward(aname, reward)
@@ -201,3 +201,85 @@ class ThompsonSampling(MultiArmBandit):
 		else:
 			sf[1] += 1
 		self.logger.info("action {} beta distr factors {} {}".format(aname, sf[0], sf[1]))	
+		
+class ExponentialWeight(MultiArmBandit):
+	"""
+	exponential weight multi arm bandit (exp3)
+	"""
+	
+	def __init__(self, actions, wsize, transientAction,logFilePath, logLevName, gama=0.2):
+		"""
+		initializer
+		
+		Parameters
+			actions : action names
+			wsize : reward window size
+			transientAction ; if decision involves some tied up resource it should be set False
+			logFilePath : log file path set None for no logging
+			logLevName : log level e.g. info, debug
+			gama : uniform distr factor
+		"""
+		assertWithinRange(gama, 0, 0.5, "gama should not be greater that 0.5")
+		self.weights = list(map(lambda a : [a, 1.0], actions))
+		self.gama = gama
+		self.naction = len(actions)
+		self.distr = None
+		self.sampler = None
+		self.__getActionDistr()
+		super(ExponentialWeight, self).__init__(actions, wsize, transientAction,logFilePath, logLevName, __name__, "ExponentialWeight")
+
+	def act(self):
+		"""
+		next play return selected action
+		"""
+		sact = None
+		sc = 0
+		for act in self.actions:
+			#any action not tried yet
+			if act.nplay == 0:
+				sact = act
+				self.logger.info("untried action found")
+				break
+		
+		if sact is None:
+			#sample reward
+			aname = self.sampler.sample()
+			for act in self.actions:
+				if act.name == aname:
+					sact = act
+					break
+		
+		self.actFinalize(sact, sc)
+		re = (sact.name, sc)	
+		return re
+
+	def setReward(self, aname, reward):
+		"""
+		reward feedback for action
+			
+		Parameters
+			aname : action name
+			reward : reward value
+		"""
+		super().setReward(aname, reward)
+		for d in self.distr:
+			if d[0] == aname:
+				rn = reward / d[1]
+				break
+				
+		for w in self.weights:
+			if w[0] == aname:
+				w[1] *= math.exp(self.gama * rn / self.naction)
+				break
+		self.__getActionDistr()
+	
+	def __getActionDistr(self):
+		"""
+		action probability distribution
+		"""	
+		tw = 0
+		for w in self.weights:
+			tw += w[1]
+		self.distr = list(map(lambda w : (w[0], (1.0 - self.gama) * w[1] / tw + self.gama / self.naction), self.weights))
+		self.sampler = CategoricalRejectSampler(self.distr)
+			
